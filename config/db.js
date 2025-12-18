e8d31de3774180c16dd00ed8af26fcf0f897d334
[Rishikesh Mishra] <[rishikesh403pat@gmail.com]>
2025-12-18 13:52:47 +0530

    Fix Railway port and MongoDB connection

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
-    console.error("MongoDB connection failed");
+    console.error("MongoDB connection failed", error.message);
     process.exit(1);
   }
 };
